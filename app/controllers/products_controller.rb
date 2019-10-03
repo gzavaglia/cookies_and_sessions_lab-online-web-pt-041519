4a6b8c26af3daaa7a672eec9317b088041bd4f89
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-10-03 17:18:21 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,30 +1,10 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
   end
-  
-  def show
-    @product = Product.find(params[:id])
-  end
-  
-  def new 
-    @product = Product.new
-  end
-  
-  def create
-    @product = Product.new(product_params)
-    redirect_to products_path
-  end
-  
+
   def add
     cart << params[:product]
-    render :index
-  end
-  
-  private
-  
-  def product_params
-    params.require(:product).permit(:name)
+    redirect_to '/'
   end
   
 end
